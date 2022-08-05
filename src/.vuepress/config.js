27a2f19ee0dbc1f241f7bcffb1695cf8d335784e
ahzvenol <ahzvenol@users.noreply.github.com>
2022-08-05 21:15:10 +0800

    文档更新

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
             description: ''
         }
     },
-    dest: './dist',
+    dest: './docs',
     extraWatchFiles: [],
     // theme: '@vuepress-theme-default', // 主题
     themeConfig: {
